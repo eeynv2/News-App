349f0b0e55e1fbc5b3f2d787a5e2cfe6630b0005
jscoaching <scionoflionhearted@gmail.com>
2024-06-06 22:36:40 +0530

    Fixed the Loop

--- src/App.js
+++ src/App.js
@@ -4,29 +4,23 @@ import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [newsItem, setNewsItem] = useState(null);
+  const [newsItem, setNewsItem] = useState({ articles: [] });
 
   useEffect(() => {
     fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=3aee9847ad0a41f99e125b988556e3fe')
-        .then(response => response.json())
-        .then(json => setNewsItem(json))
-        .catch(error => console.error(error));
-
-}, []);
-let newsArray = newsItem?.articles;
+      .then(response => response.json())
+      .then(json => setNewsItem(json))
+      .catch(error => console.error(error));
+  }, []);
 
   return (
     <div className="App">
       <Navbar firstNav={"Home"} secondNav={"News"}></Navbar>
       <div className="container">
         <div className="row">
-          {(() => {
-            let cards = [];
-            for (let i = 1; i <= newsArray.length; i++) {
-              cards.push(<Card key={i} title = {i.title}></Card>);
-            }
-            return cards;
-          })()}
+          {newsItem.articles.map((article, index) => (
+            <Card key={index} title={article.title}></Card>
+          ))}
         </div>
       </div>
     </div>
